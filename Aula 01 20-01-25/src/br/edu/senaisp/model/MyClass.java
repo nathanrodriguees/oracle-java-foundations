3f1dd7703ee199ad475840e60d79d87ae3ce07d3
nathanrodriguees <nathan_rodrigues07@outlook.com>
2025-01-20 21:48:10 -0300

    feat: Aula 01

---
+++ Aula 01 20-01-25/src/br/edu/senaisp/model/MyClass.java
@@ -0,0 +1,15 @@
+package br.edu.senaisp.model;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+
+		int joao = 10;
+	    int nathan = 25;
+	    int matheus = 22;
+	    int soma = joao + nathan + matheus;
+
+	    System.out.println("A soma das idades " + joao + "+" + nathan + "+" + matheus + "=" + soma);
+	}
+
+}
