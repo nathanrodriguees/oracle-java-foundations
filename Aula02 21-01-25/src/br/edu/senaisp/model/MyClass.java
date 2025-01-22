f56f94629450afec058e74f136703d4a27c51138
nathanrodriguees <nathan_rodrigues07@outlook.com>
2025-01-21 21:00:01 -0300

    feat: Operador ternario com mais condicional

--- Aula02 21-01-25/src/br/edu/senaisp/model/MyClass.java
+++ Aula02 21-01-25/src/br/edu/senaisp/model/MyClass.java
@@ -4,17 +4,30 @@ public class MyClass {
 
 	public static void main(String[] args) {
 		
-		int idade = 19;
-		
-//		if( idade >= 18) {
+//		int idade = 17;
+//		
+//		boolean ehMaior = (idade >= 18);
+//		
+//		if( ehMaior) {
 //			System.out.println("Maior de idade");
 //		}else{
 //			System.out.println("Menor de idade");
 //		}
 	     
-		String mensagem = idade < 18
-				? "Menor de idade"
-				: "Maior de idade";
+//		String mensagem = idade < 18
+//				? "Menor de idade"
+//				: "Maior de idade";
+//		
+//		System.out.println(mensagem);
+		
+		int idade = 11;
+
+		String mensagem = idade < 12 
+		        ? "Criança" 
+		        : idade < 18 
+		            ? "Adolescente" 
+		            : "Adulto";
+
 		System.out.println(mensagem);
 	}
 }
