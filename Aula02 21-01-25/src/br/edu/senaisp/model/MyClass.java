4b0982f9fe6b62afc5dccfa319796ae20613a151
nathanrodriguees <nathan_rodrigues07@outlook.com>
2025-01-21 21:28:50 -0300

    feat: Scanner e mais formas de imprimir resultado

--- Aula02 21-01-25/src/br/edu/senaisp/model/MyClass.java
+++ Aula02 21-01-25/src/br/edu/senaisp/model/MyClass.java
@@ -1,8 +1,11 @@
 package br.edu.senaisp.model;
+import java.util.*;
 
 public class MyClass {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
 		
 //		int idade = 17;
 //		
@@ -13,21 +16,43 @@ public class MyClass {
 //		}else{
 //			System.out.println("Menor de idade");
 //		}
-	     
+//		
+//		// Faça um programa que receba 2 valores e escreva o maior deles.
+//		
+		System.out.printf("Digite o 1° número:");
+		int n1 = sc.nextInt();
+		
+		System.out.printf("Digite o 2° número:");
+		int n2 = sc.nextInt();
+		
+		if (n1 > n2) {
+			System.out.printf("%d é maior\n", n1);
+			System.out.println(String.format("%d é maior", n1));
+		}else {
+			if (n1 == n2) {
+				System.out.println(n1 + " e " + n2 + " são iguais");
+			}else {
+				System.out.printf("%d é maior\n", n2);
+				System.out.println(String.format("%d é maior", n2));
+			}
+		}
+		
+		
+//
 //		String mensagem = idade < 18
 //				? "Menor de idade"
 //				: "Maior de idade";
 //		
 //		System.out.println(mensagem);
-		
-		int idade = 11;
-
-		String mensagem = idade < 12 
-		        ? "Criança" 
-		        : idade < 18 
-		            ? "Adolescente" 
-		            : "Adulto";
-
-		System.out.println(mensagem);
+//		
+//		int idade = 11;
+//
+//		String mensagem = idade < 12 
+//		        ? "Criança" 
+//		        : idade < 18 
+//		            ? "Adolescente" 
+//		            : "Adulto";
+//
+//		System.out.println(mensagem);
 	}
 }
