5cb626545b84df755c6901861d6754dd7378c32d
nathanrodriguees <nathan_rodrigues07@outlook.com>
2025-01-28 19:17:52 -0300

    feat: Laços de repetição

---
+++ Aula06 27-01-25/src/br/edu/senaisp/MyClass.java
@@ -0,0 +1,71 @@
+package br.edu.senaisp;
+
+public class MyClass {
+
+//	Exercício para numeros impares:
+//	public static void main(String[] args) {
+//		
+//		 for (int i = 0; i <= 11; i++) {
+//	       if (i % 2 == 1) {
+//	         System.out.println(i);
+//	       }
+//	     }
+//	    
+//	    for (int i = 1; i < 20; i = i + 2) {
+//	        System.out.println(i);
+//	    }
+//
+//	}
+
+//	Exercício do triangulo:
+//	public static void main(String[] args) {
+//	      Scanner xaxa = new Scanner(System.in);
+//	      
+//	      int lados[] = new int[3];
+//	      String triangulo = "";
+//	    
+//	      
+//	      for(int i = 0; i < 3; i++) {
+//	        lados[i] = xaxa.nextInt();
+//	      }
+//	      
+//	      boolean equilatero = lados[0] == lados[1] && lados[1] == lados[2];
+//	      boolean isosceles = lados[0] == lados[1] || lados[1] == lados[2] || lados[0] == lados[2];
+//	      
+//	      if (equilatero) {
+//	        triangulo = "Equilátero";
+//	      } else if(isosceles) {
+//	        triangulo = "Isósceles";
+//	      } else {
+//	        triangulo = "Escaleno";
+//	      }
+//	      System.out.println("O triângulo é do tipo " + triangulo);
+//	  }
+
+//	public static void main(String[] args) {
+//	      
+//	      int i = 1;
+//	      
+//	      while(i <= 30){
+//	        System.out.print(i);
+//	      
+//	        if(i % 5 == 0) {
+//	          System.out.println(" xaxa");
+//	        }  
+//	        i++;
+//	        System.out.println(" ");
+//	      }
+//	      
+//	    
+//	  }
+
+//	public static void main(String[] args) {
+//
+//		for (int i = 8; i <= 42; i++)
+//			System.out.println((i % 2 == 1) ? i : "");
+//
+//		if (i % 2 == 1) {
+//			System.out.println(i);
+//		}
+//	}
+}
