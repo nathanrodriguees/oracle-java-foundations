69edc070ddc8774aefc119e4e0b16c36139f3f13
nathanrodriguees <nathan_rodrigues07@outlook.com>
2025-01-28 21:50:10 -0300

    feat: Eclipse Start

---
+++ Aula07 28-01-25/src/br/edu/senaisp/Triangulo.java
@@ -0,0 +1,50 @@
+package br.edu.senaisp;
+
+import java.util.Scanner;
+
+public class Triangulo {
+
+//	public static void main(String[] args) {
+//
+////	Exercício do triangulo:
+//		Scanner xaxa = new Scanner(System.in);
+//
+//		int lados[] = new int[3];
+//		String triangulo = "";
+//
+//		for (int i = 0; i < 3; i++) {
+//			lados[i] = xaxa.nextInt();
+//		}
+//
+//		boolean equilatero = lados[0] == lados[1] && lados[1] == lados[2];
+//		boolean isosceles = lados[0] == lados[1] || lados[1] == lados[2] || lados[0] == lados[2];
+//
+//		if (equilatero) {
+//			triangulo = "Equilátero";
+//		} else if (isosceles) {
+//			triangulo = "Isósceles";
+//		} else {
+//			triangulo = "Escaleno";
+//		}
+//		System.out.println("O triângulo é do tipo " + triangulo);
+//
+//	}
+
+	public static void main(String[] args) {
+
+//		Exercício do triangulo:
+
+		int l1 = 6;
+		int l2 = 6;
+		int l3 = 6;
+
+		if (l1 == l2 && l2 == l3) {
+			System.out.println("Equilátero!");
+		} else if( l1 != l2 && l2 != l3 && l1 != l3){
+			System.out.println("Escaleno!");
+		} else {
+			System.out.println("Isosceles!");
+		}
+
+	}
+}
