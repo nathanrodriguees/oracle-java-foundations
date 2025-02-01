bd483db965b616c40034acfd1e7b2fd9416e103c
nathanrodriguees <nathan_rodrigues07@outlook.com>
2025-01-31 21:13:22 -0300

    feat: Criação do metodo para calcular media das alturas dos alunos

--- Aula10 31-01-25/src/Cadastro.java
+++ Aula10 31-01-25/src/Cadastro.java
@@ -5,37 +5,36 @@
 // 3. Gere mais 4 vetores String com os respectivos nomes: nome[], altura[], idade[], ciade[]
 // 4. Utilizando a função split da classe String separe as informações conforme os vetores corretos
 
-public class Exercicio {
+public class Cadastro {
 
 	public static void main(String[] args) {
 
 		// 1. Gere um vetor de String com 3 posições
-		String[] aluno = new String[3];
+		String[] alunos = new String[3];
 
 		// 2. Escolha 3 colegas e em cada posição do vetor registre-os;
-		aluno[0] = "João;1.78;19;São Paulo";
-		aluno[1] = "Matheus;1.90;22;São Paulo";
-		aluno[2] = "Nathan;1.81;24;São Paulo";
-		
-		// 3. Gere mais 4 vetores String com os respectivos nomes: 
+		alunos[0] = "João;1.78;19;São Paulo";
+		alunos[1] = "Matheus;1.90;22;São Paulo";
+		alunos[2] = "Nathan;1.81;24;São Paulo";
+
+		// 3. Gere mais 4 vetores String com os respectivos nomes:
 		// nome[], altura[], idade[], cidade[]
 		String[] nome = new String[3];
 		String[] altura = new String[3];
 		String[] idade = new String[3];
 		String[] cidade = new String[3];
-		
-		
-		// 4. Utilizando a função split da classe String separe as 
+
+		// 4. Utilizando a função split da classe String separe as
 		// informações conforme os vetores corretos
-		for (int i = 0; i < aluno.length; i++) {
-			
-			String[] vetor = aluno[i].split(";");
-			
+		for (int i = 0; i < alunos.length; i++) {
+
+			String[] vetor = alunos[i].split(";");
+
 			nome[i] = vetor[0];
 			altura[i] = vetor[1];
 			idade[i] = vetor[2];
 			cidade[i] = vetor[3];
-			
+
 			System.out.print(nome[i] + "; ");
 			System.out.print(altura[i] + "; ");
 			System.out.print(idade[i] + "; ");
@@ -43,6 +42,19 @@ public class Exercicio {
 			System.out.println("\n");
 		}
 
+		float media = mediaAltura(altura);
+		System.out.printf("Media da Altura: = %.2f", media);
+	}
+
+	// Metodo para calcular a media da altura dos alunos
+	public static float mediaAltura(String[] altura) {
+		float somatorio = 0;
+
+		for (int i = 0; i < altura.length; i++) {
+			float temp = Float.parseFloat(altura[i]);
+			somatorio += temp;
+		}
+		return somatorio / altura.length;
 	}
 
 }
