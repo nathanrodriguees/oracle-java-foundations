ccd4414ceff82354f554530e3ed60afb2f8b2739
nathanrodriguees <nathan_rodrigues07@outlook.com>
2025-02-05 21:53:39 -0300

    feat: Criando uma Oficina de Carros

---
+++ Aula13 05-02-25/src/br/edu/senaisp/Oficina/Carro.java
@@ -0,0 +1,12 @@
+package br.edu.senaisp.Oficina;
+
+public class Carro {
+	
+	String nome;
+	String marca;
+	float km;
+	String cor;
+	String placa;
+	String nomeCliente;
+
+}
---
+++ Aula13 05-02-25/src/br/edu/senaisp/Oficina/Teste.java
@@ -0,0 +1,17 @@
+package br.edu.senaisp.Oficina;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		Carro c1 = new Carro();
+		c1.nome = "Fusca";
+		c1.marca = "Volkswagen";
+		c1.cor = "Azul Bebê";
+		c1.km = 1_000_000;
+		c1.placa = "EPG0X12";
+		c1.nomeCliente = "Armando Peixoto";
+
+	}
+
+}
