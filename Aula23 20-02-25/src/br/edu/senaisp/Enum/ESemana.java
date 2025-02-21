19e0f486cf64dc46300c7c1ec413d5af2feb1383
nathanrodriguees <nathan_rodrigues07@outlook.com>
2025-02-20 21:52:17 -0300

    feat: Introdução a ENUM

---
+++ Aula23 20-02-25/src/br/edu/senaisp/Enum/ESemana.java
@@ -0,0 +1,5 @@
+package br.edu.senaisp.Enum;
+
+public enum ESemana {
+	DOMINGO, SEGUNDA, TERCA, QUARTA, QUINTA, SEXTA, SABADO
+}
---
+++ Aula23 20-02-25/src/br/edu/senaisp/Enum/Teste.java
@@ -0,0 +1,12 @@
+package br.edu.senaisp.Enum;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		ESemana diaDaSemana = ESemana.QUINTA;
+	}
+
+	public static void qualquerCoisa(ESemana s) {
+
+	}
+}
