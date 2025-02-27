fa199b6793729c3dae825816bf643053a3541c50
nathanrodriguees <nathan_rodrigues07@outlook.com>
2025-02-26 21:45:37 -0300

    feat: Tratativa de erro com Try, Catch e Throw(s)

--- Aula27 26-02-25/src/Teste.java
+++ Aula27 26-02-25/src/Teste.java
@@ -1,9 +1,37 @@
+import java.util.Scanner;
 
 public class Teste {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Digite o 1º Nr: ");
+		int a = sc.nextInt();
+		System.out.println("Digite o 2º Nr: ");
+		int b = sc.nextInt();
+		double resultado = 0;
+
+		// Tratando o erro
+		try {
+			resultado = dividir(a, b);
+		} catch (Exception e) {
+			System.out.println(e.getMessage() + "Erro");
+		}
+
+		System.out.println(resultado);
 
 	}
 
+	// Camada intermediaria
+	public static double calculeira(int a, int b) throws Exception {
+		return dividir(a, b);
+	}
+
+	// Camada para lançar o erro
+	public static double dividir(int a, int b) {
+		if (b == 0)
+			throw new ArithmeticException("Não pode dividir por zero");
+
+		return a / b;
+	}
 }
